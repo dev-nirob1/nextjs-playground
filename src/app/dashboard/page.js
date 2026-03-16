7c5b563d6211b6dd90eb86eb7149e92d58181b3f
dev-nirob1 <dev.nirob1@gmail.com>
2026-03-16 16:37:25 +0600

    routing

---
+++ src/app/dashboard/page.js
@@ -0,0 +1,8 @@
+export default function DashbboardPage() {
+  return (
+    <div className="dashboard-page">
+      <h2>Welcome to the Dashboard</h2>
+      <p>This is the main page of the dashboard.</p>
+    </div>
+  );
+}
